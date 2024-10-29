0f93e85b3cc52371348702aad213c683c8011839
adozaraz <minecraft-no.adozaraz@yandex.ru>
2024-10-29 23:01:21 +0400

    Implement controller

--- src/main/java/ru/ssau/blazebankcardservice/controller/CardController.java
+++ src/main/java/ru/ssau/blazebankcardservice/controller/CardController.java
@@ -1,10 +1,30 @@
 package ru.ssau.blazebankcardservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.ssau.blazebankcardservice.entities.CardDTO;
+import ru.ssau.blazebankcardservice.service.CardService;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/card")
 public class CardController {
+    @Autowired
+    private CardService cardService;
 
+    @PostMapping("/save")
+    public void createNewCard(@RequestBody CardDTO cardDTO) {
+        cardService.addNewCard(cardDTO);
+    }
+
+    @GetMapping("/get/id")
+    public CardDTO getCardById(@RequestParam UUID uuid) {
+        return cardService.getCardDTOById(uuid);
+    }
+
+    @GetMapping("/get/userId")
+    public CardDTO getCardByUserId(@RequestParam UUID userId) {
+        return cardService.getCardDTOByUserId(userId);
+    }
 }
